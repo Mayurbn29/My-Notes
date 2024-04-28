4711ee60f0f92ff56c7b1bae3b2e76013c2b9418
mayur2281 <mayur@blu-ocean.net>
2024-04-28 13:02:52 +0530

    Quartz sync: Apr 28, 2024, 1:02 PM

--- quartz/plugins/transformers/lastmod.ts
+++ quartz/plugins/transformers/lastmod.ts
@@ -5,11 +5,11 @@ import { QuartzTransformerPlugin } from "../types"
 import chalk from "chalk"
 
 export interface Options {
-  priority: ("frontmatter")[]
+  priority: ("frontmatter" | "git")[]
 }
 
 const defaultOptions: Options = {
-  priority: ["frontmatter"],
+  priority: ["frontmatter", "git"],
 }
 
 function coerceDate(fp: string, d: any): Date {
